0b6983f543cf673d44d75aff59a2453efaad6883
Dawood Ayaz <dawoodayaz18@gmail.com>
2025-03-29 06:52:40 +0500

    created homepage for buns&burger.

---
+++ src/pages/homepage/homePage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeItems from '../../components/homeItems/home-items.component';
+
+
+const HomePage = () => {
+    return (
+    <div>
+        <h1 className="text-red-700 font-bold text-4xl mt-40 text-center">Burgers</h1>
+    <HomeItems />
+    </div>
+    )
+}
+
+export default HomePage
