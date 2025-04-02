afb862b66bcb41ee474f90401628770efcc12759
Dawood Ayaz <dawoodayaz18@gmail.com>
2025-04-03 02:33:08 +0500

    making a cart page.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/homepage/homePage.component';
 import Header from './components/header/header.component';
-import MenuPage from './pages/menupage/menu.component';
+import MenuPage from './pages/menupage/menupage.component';
 import Footer from './components/footer/footer.component';
+import CartPage from './pages/cartpage/cartpage.component';
+import { CartProvider } from './components/cart/cartcontext';
 import './App.css'
 
 
@@ -17,16 +19,19 @@ class App extends React.Component {
   }
   render() {
     return (
-      <Router>
+      <CartProvider>
+        <Router>
       <div>
       <Header />
         <Routes>
           <Route path='/' element={ <HomePage /> } />
           <Route path='/menu' element={ <MenuPage /> } />
+          <Route path='/cart' element={ <CartPage /> } />
         </Routes>
         <Footer />
         </div>
       </Router>
+      </CartProvider>
     )
   }
 }
